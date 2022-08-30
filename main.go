b03e3c9694d8add7a3422f9d7ac01b1f4efda5f8
Thomas M Guarnieri <thomasmguarnieri@gmail.com>
2022-08-29 23:06:03 -0300

    first: ctrl c and v Server imp from cirocosta

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"net"
+	"time"
 )
 
 // maxBufferSize specifies the size of the buffers that
 // are used to temporarily hold data from the UDP packets
 // that we receive.
 const maxBufSize = 1024
+const timeout = 15 * time.Second
 
 func server(ctx context.Context, addr string) error {
 	doneCh := make(chan error, 1)
@@ -17,14 +19,66 @@ func server(ctx context.Context, addr string) error {
 
 	pc, err := net.ListenPacket("udp", addr)
 	if err != nil {
-		log.Panicln(err)
+		return err
 	}
 
 	defer pc.Close()
 
+	go func() {
+		for {
+			// Read from client.
+			n, a, err := pc.ReadFrom(buf)
+			if err != nil {
+				doneCh <- err
+				return
+			}
+
+			fmt.Printf("packet-received: bytes=%d from=%s\n", n, a.String())
+
+			err = pc.SetWriteDeadline(time.Now().Add(timeout))
+			if err != nil {
+				doneCh <- err
+				return
+			}
+
+			// Write the packet's content back to the client.
+			n, err = pc.WriteTo(buf[:n], a)
+			if err != nil {
+				doneCh <- err
+				return
+			}
+
+			fmt.Printf("packet-written: bytes=%d to=%s\n", n, a.String())
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("\tServer says: bye!")
+		err = ctx.Err()
+	case <-doneCh:
+		return err
+	}
+
+	return nil
+}
+
+func client(ctx context.Context, addr string) error {
+	doneCh := make(chan error, 1)
+
+	raddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.DialUDP("udp", nil, raddr)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
 	//go func() {
-	//	for {
 	//
-	//	}
 	//}()
 }
